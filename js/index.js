9210493acca84b69d0c247b636d2299b1c244785
danwainwright <daniel.wainwright@bbc.co.uk>
2016-08-19 10:56:27 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var url = 'https://raw.githubusercontent.com/danwainwright/Flightdelays2/master/convertcsv%20(1).json';
+$.getJSON(url, function(response){  
+  $('#flights').DataTable({
+    'data' : response,
+    'columns': [
+      {'data' : 'reporting_airport'},
+      {'data' : 'origin_destination_country'},
+      {'data' : 'origin_destination'},
+      {'data' : 'airline_name'},
+      {'data' : 'average_delay_mins'},
+      {'data' : 'reporting_period'},
+    ]
+  });
+})
